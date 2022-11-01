f4a691c8c0987dc6eb8f9bf5ec02b3b7de9e0f51
eastmin <imsenhaku@gmail.com>
2022-11-01 12:35:48 +0900

    1. 테스트

---
+++ src/main/java/peer/MybatisConfig.java
@@ -0,0 +1,47 @@
+package peer;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+public class MybatisConfig {
+	
+	@MapperScan /* Mapper 어노테이션 인식을 위해서 반드시 기입 - 에러발생 이유 */
+	@Configuration /* 환경설정 어노테이션 */
+	@PropertySource("classpath:/application.properties") /* Spring Property 정보 주입 */
+	public class DataAccessConfig {
+		
+		@ConfigurationProperties(prefix = "spring.datasource")    
+		public DataSource dataSource() {
+			return DataSourceBuilder.create().build();
+		}
+		
+		@Bean
+		public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+			SqlSessionFactoryBean factoryBean=new SqlSessionFactoryBean();
+			
+			factoryBean.setDataSource(dataSource);
+			factoryBean.setMapperLocations(
+					new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/*.xml")
+					);
+			
+			factoryBean.setTypeAliasesPackage("peer.model"); /* DTO Alias 설정 */
+			return factoryBean.getObject();
+		}
+		
+		@Bean
+		public SqlSessionTemplate sessionTemplate(SqlSessionFactory sqlSessionFactory) {
+			return new SqlSessionTemplate(sqlSessionFactory);
+		}
+	}
+
+}
